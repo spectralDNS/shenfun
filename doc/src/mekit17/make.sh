bf05c63f8809a7d1e421863771933145279057f9
Mikael Mortensen <mikaem@math.uio.no>
2017-08-11 22:55:13 +0200

    On html documentation

--- doc/src/mekit17/make.sh
+++ doc/src/mekit17/make.sh
@@ -5,10 +5,15 @@ name=shenfun
 doconce format pdflatex $name "--latex_code_style=default:lst[style=yellow2_fb]@sys:lst-gray"
 pdflatex $name
 bibtex $name
+bibtex $name
 pdflatex $name
 
 doconce format html $name --html_style=bootswatch_readable --html_code_style=inherit --html_output=shenfun
 doconce format html $name --html_style=solarized3 --html_output=shenfun-solarized
+
+style=bootstrap
+doconce format html $name --html_style=${style} --pygments_html_style=default --html_admon=bootstrap_alert --html_output=shenfun_${style} --keep_pygments_html_bg —html_code_style=inherit --html_pre_style=inherit
+doconce split_html shenfun_${style}.html
 
 doconce format sphinx $name
 theme=cloud
@@ -23,6 +28,8 @@ cp -r sphinx-rootdir/_build/html $dest
 cp $name.pdf $dest/shenfun.pdf
 cp -r figs $dest
 cp shenfun*.html $dest
+cp ._shenfun_bootstrap* $dest
+cp .shenfun_bootstrap* $dest
 
 # Add to git if new files have been created
 git add $dest
