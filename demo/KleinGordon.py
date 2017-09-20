ce7f1f11dc02914bd228e268af07125ac3cbee4c
Mikael Mortensen <mikaem@math.uio.no>
2017-09-20 21:32:14 +0200

    https in kleingordon demo

--- demo/KleinGordon.py
+++ demo/KleinGordon.py
@@ -98,12 +98,12 @@ a = [1./6., 1./3., 1./3., 1./6.]         # Runge-Kutta parameter
 b = [0.5, 0.5, 1.]                       # Runge-Kutta parameter
 t = 0.0
 dt = 0.005
-end_time = 1.
+end_time = 10.
 tstep = 0
 #levels = np.linspace(-0.06, 0.1, 100)/8
-if rank == -1:
+if rank == 0:
     plt.figure()
-    image = plt.contourf(X[0][:, :, 0], X[1][:, :, 0], u[:, :, 16], 100)
+    image = plt.contourf(X[1][:, :, 0], X[0][:, :, 0], u[:, :, 16], 100)
     plt.draw()
     plt.pause(1e-4)
 t0 = time()
@@ -121,7 +121,7 @@ while t < end_time-1e-8:
         uf_hat1 += a[rk]*dt*duf
     uf_hat[:] = uf_hat1
 
-    if tstep % 5 == -1:
+    if tstep % 100 == 0:
         uf = TT.backward(uf_hat, uf)
         ekin = 0.5*energy_fourier(T.comm, np.array(N), f_hat)
         es = 0.5*energy_fourier(T.comm, np.array(N), 1j*K*u_hat)
@@ -132,7 +132,7 @@ while t < end_time-1e-8:
         ea = comm.allreduce(np.sum(np.array(X)*(0.5*f**2 + 0.5*gradu**2 - (0.5*u**2 - 0.25*u**4)*f))/np.prod(np.array(N)))
         if rank == 0:
             image.ax.clear()
-            image.ax.contourf(X[0][:, :, 0], X[1][:, :, 0], u[:, :, 16], 100)
+            image.ax.contourf(X[1][:, :, 0], X[0][:, :, 0], u[:, :, 16], 100)
             plt.pause(1e-6)
             #plt.savefig('Klein_Gordon_{}_real_{}.png'.format(N[0], tstep))
             print("Time = %2.2f Total energy = %2.8e Linear momentum %2.8e Angular momentum %2.8e" %(t, ekin+es+eg, ep, ea))
