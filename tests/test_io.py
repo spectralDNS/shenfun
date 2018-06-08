a1f962fe8182a648914d93edeffda4664c33a8f0
Aurélien Ponte <aponte@br146-058.ifremer.fr>
2018-06-08 16:17:41 +0200

    start implementing nc tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -52,3 +52,46 @@ def test_mixed_3D():
     hfile.close()
     generate_xdmf('h5test4.h5')
 
+def test_nc_regular_2D():
+    K0 = Basis(N[0], 'F')
+    K1 = Basis(N[1], 'C')
+    T = TensorProductSpace(comm, (K0, K1))
+    ncfile = NCWriter('nctest.nc', ['u'], T, clobber=True)
+    u = Array(T, val=1)
+    ncfile.write_tstep(0, u)
+    ncfile.close()
+
+def test_nc_mixed_2D():
+    K0 = Basis(N[0], 'F')
+    K1 = Basis(N[1], 'C')
+    T = TensorProductSpace(comm, (K0, K1))
+    TT = MixedTensorProductSpace([T, T])
+    ncfile = NCWriter('nctest2.nc', ['u', 'f'], TT)
+    uf = Array(TT, val=2)
+    ncfile.write_tstep(0, uf)
+    ncfile.close()
+
+def test_nc_regular_3D():
+    K0 = Basis(N[0], 'F', dtype='D')
+    K1 = Basis(N[1], 'F', dtype='d')
+    K2 = Basis(N[2], 'C')
+    T = TensorProductSpace(comm, (K0, K1, K2))
+    ncfile = HDF5Writer('nctest3.nc', ['u'], T)
+    u = Array(T, val=1)
+    ncfile.write_tstep(0, u)
+    #ncfile.write_slice_tstep(0, [slice(None), 4, slice(None)], u)
+    #ncfile.write_slice_tstep(0, [slice(None), 4, 4], u)
+    ncfile.close()
+
+def test_nc_mixed_3D():
+    K0 = Basis(N[0], 'F', dtype='D')
+    K1 = Basis(N[1], 'F', dtype='d')
+    K2 = Basis(N[2], 'C')
+    T = TensorProductSpace(comm, (K0, K1, K2))
+    TT = MixedTensorProductSpace([T, T])
+    ncfile = NCWriter('nctest4.nc', ['u', 'f'], TT)
+    uf = Array(TT, val=2)
+    ncfile.write_tstep(0, uf)
+    #ncfile.write_slice_tstep(0, [slice(None), 4, slice(None)], uf)
+    #ncfile.write_slice_tstep(0, [slice(None), 4, 4], uf)
+    ncfile.close()
