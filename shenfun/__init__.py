95ce689eb20168dbab916c2a651fcb546b8d1015
Mikael Mortensen <mikaem@math.uio.no>
2018-11-07 13:51:37 +0100

    Bump version number

--- shenfun/__init__.py
+++ shenfun/__init__.py
@@ -13,7 +13,7 @@ parallelized with MPI through the
 """
 #pylint: disable=wildcard-import,no-name-in-module
 
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 __author__ = 'Mikael Mortensen'
 
 import numpy as np
