0c9f4e59756ceb46ec023310a2ec6e29ce66c982
Mikael Mortensen <mikaem@math.uio.no>
2019-02-08 14:32:46 +0100

    Remove sphinx_bootstrap_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 import subprocess
-import sphinx_bootstrap_theme
+#import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
