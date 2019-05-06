0d78cfe7cbb747a2bf713e25b9d4fd6f1b9cb213
Mikael Mortensen <mikaem@math.uio.no>
2019-05-06 10:56:35 +0200

    Adding outer.pyx

---
+++ shenfun/optimization/cython/outer.pyx
@@ -0,0 +1,35 @@
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: language_level=3
+
+import numpy as np
+cimport cython
+cimport numpy as np
+
+ctypedef fused T:
+    np.float64_t
+    np.complex128_t
+
+def outer2D(T[:, :, ::1] a, T[:, :, ::1] b, T[:, :, ::1] c):
+    cdef int i, j
+    for i in range(a.shape[1]):
+        for j in range(a.shape[2]):
+            c[0, i, j] = a[0, i, j]**2           # (0, 0)
+            c[1, i, j] = a[0, i, j]*a[1, i, j]   # (0, 1)
+            c[2, i, j] = c[1, i, j]              # (1, 0)
+            c[3, i, j] = a[1, i, j]**2           # (1, 1)
+
+def outer3D(T[:, :, :, ::1] a, T[:, :, :, ::1] b, T[:, :, :, ::1] c):
+    cdef int i, j, k
+    for i in range(a.shape[1]):
+        for j in range(a.shape[2]):
+            for k in range(a.shape[3]):
+                c[0, i, j, k] = a[0, i, j, k]*b[0, i, j, k]   # (0, 0)
+                c[1, i, j, k] = a[0, i, j, k]*b[1, i, j, k]   # (0, 1)
+                c[2, i, j, k] = a[0, i, j, k]*b[2, i, j, k]   # (0, 2)
+                c[3, i, j, k] = c[1, i, j, k]                 # (1, 0)
+                c[4, i, j, k] = a[1, i, j, k]*b[1, i, j, k]   # (1, 1)
+                c[5, i, j, k] = a[1, i, j, k]*b[2, i, j, k]   # (1, 2)
+                c[6, i, j, k] = c[2, i, j, k]                 # (2, 0)
+                c[7, i, j, k] = c[5, i, j, k]                 # (2, 1)
+                c[8, i, j, k] = a[2, i, j, k]*b[2, i, j, k]   # (2, 2)
