16fbb9948109f16003b80c55a06f51bfcb0dfeeb
Mikael Mortensen <mikaem@math.uio.no>
2019-05-08 14:44:39 +0200

    Bump version

--- shenfun/__init__.py
+++ shenfun/__init__.py
@@ -25,7 +25,7 @@ routines to `Cython <https://cython.org/>`_.
 """
 #pylint: disable=wildcard-import,no-name-in-module
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 __author__ = 'Mikael Mortensen'
 
 import numpy as np
