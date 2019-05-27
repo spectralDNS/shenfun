8741cf23b3a9fd42c5ea5e354ed2927fa289033d
Mikael Mortensen <mikaem@math.uio.no>
2019-05-27 23:18:12 +0800

    Try with build target 10.13

--- conf/conda/build.sh
+++ conf/conda/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#if [ "$(uname)" == "Darwin" ]
-#then
-#    export CXXFLAGS="-stdlib=libc++ ${CXXFLAGS}"
-#    export LDFLAGS="-Wl,-rpath,$PREFIX/lib"
-#    export MACOSX_DEPLOYMENT_TARGET=10.0
-#fi
+if [ "$(uname)" == "Darwin" ]
+then
+    export CXXFLAGS="-stdlib=libc++ ${CXXFLAGS}"
+    export LDFLAGS="-Wl,-rpath,$PREFIX/lib"
+    export MACOSX_DEPLOYMENT_TARGET=10.13
+fi
 
 #pip install --no-deps --no-binary :all: -r "${RECIPE_DIR}/component-requirements.txt"
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
