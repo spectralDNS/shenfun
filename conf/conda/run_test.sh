7df84679fcae537cfb2dc4114f06fa38bf50bf20
Mikael Mortensen <mikaem@math.uio.no>
2019-07-05 15:21:58 +0200

    Making quadpy optional

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -4,4 +4,6 @@ export OMPI_MCA_plm=isolated
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 export OMPI_MCA_rmaps_base_oversubscribe=yes
 
+$PYTHON -m pip install -r "${RECIPE_DIR}/component-requirements.txt"
+
 py.test tests/
