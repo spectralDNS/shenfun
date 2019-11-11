31948999ae2ae0317dfb9906068abb815e747e40
Mikael Mortensen <mikaem@math.uio.no>
2019-11-11 16:28:23 +0100

    Remove python 3.8 build because of missing netcdf dependency

--- shenfun/chebyshev/matrices.py
+++ shenfun/chebyshev/matrices.py
@@ -1086,7 +1086,7 @@ class ADDmat(SpectralMatrix):
         # Move axis to first
         if axis > 0:
             u = np.moveaxis(u, axis, 0)
-            if not u is b:
+            if u is not b:
                 b = np.moveaxis(b, axis, 0)
 
         bs = b[s]
