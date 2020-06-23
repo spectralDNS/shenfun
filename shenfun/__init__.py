4dcb7488ded22712d680f590722d3589b298b92d
Mikael Mortensen <mikaem@math.uio.no>
2020-06-23 14:57:25 +0200

    Bump version number to 3

--- shenfun/__init__.py
+++ shenfun/__init__.py
@@ -28,7 +28,7 @@ coordinates, like polar or spherical coordinates.
 """
 #pylint: disable=wildcard-import,no-name-in-module
 
-__version__ = '2.3.0'
+__version__ = '3.0.0'
 __author__ = 'Mikael Mortensen'
 
 import numpy as np
