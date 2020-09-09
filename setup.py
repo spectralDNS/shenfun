a12a24a6cabd5777b3c3b816ee7d7e0331b8067d
Mikael Mortensen <mikaem@math.uio.no>
2020-09-09 14:03:14 +0200

    Fixing CONDA_BUILD bug in setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class build_ext_subclass(build_ext):
             extra_compile_args.append('-O0')
         else:
             extra_compile_args.append('-O3')
-            if not (os.environ.get('CONDA_BUILD', 0) == 1):
+            if not os.environ.get('CONDA_BUILD', 0):
                 for c in ['-w', '-Ofast', '-ffast-math', '-march=native']:
                     if has_flag(self.compiler, c):
                         extra_compile_args.append(c)
