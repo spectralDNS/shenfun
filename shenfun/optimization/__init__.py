d487e8d74d8bbbadf003cd128f80868cc5651d21
Mikael Mortensen <mikaem@math.uio.no>
2021-08-28 09:12:52 +0200

    Use try clause for config in optimizer

--- shenfun/optimization/__init__.py
+++ shenfun/optimization/__init__.py
@@ -18,8 +18,14 @@ except ModuleNotFoundError:
 def optimizer(func):
     """Decorator used to wrap calls to optimized versions of functions."""
     from shenfun.config import config
-    mod = config['optimization']['mode']
-    verbose = config['optimization']['verbose']
+    mod = 'cython'
+    verbose = False
+    try:
+        mod = config['optimization']['mode']
+        verbose = config['optimization']['verbose']
+    except KeyError:
+        pass
+
     if mod.lower() not in ('cython', 'numba'):
         # Use python function
         if verbose:
