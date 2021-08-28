f7b5631f513ccbe68e272a3f3319cbe8c971c5fa
Mikael Mortensen <mikaem@math.uio.no>
2021-08-28 12:14:36 +0200

    New tests with expandvars in config

--- shenfun/config.py
+++ shenfun/config.py
@@ -3,14 +3,14 @@ import yaml
 
 # The configuration file can be overloaded locally or in '~/.shenfun'
 
-print(os.path.dirname(os.path.expandvars(os.path.abspath(__file__))))
-locations = [os.path.dirname(os.path.expandvars(os.path.abspath(__file__))),
+print(os.path.expandvars(os.path.dirname((os.path.abspath(__file__)))))
+locations = [os.path.dirname(os.path.abspath(__file__)),
              os.path.expanduser('~/.shenfun'),
              os.getcwd()]
 
 config = {}
 for loc in locations:
-    fl = os.path.join(loc, 'shenfun.yaml')
+    fl = os.path.expandvars(os.path.join(loc, 'shenfun.yaml'))
     try:
         with open(fl, 'r') as yf:
             config.update(yaml.load(yf, Loader=yaml.FullLoader))
