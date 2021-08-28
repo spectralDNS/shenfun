a8958f0c5e63b6782c67cd0a6c4aaa740e83172e
Mikael Mortensen <mikaem@math.uio.no>
2021-08-28 10:39:17 +0200

    Debugging config

--- shenfun/config.py
+++ shenfun/config.py
@@ -3,6 +3,7 @@ import yaml
 
 # The configuration file can be overloaded locally or in '~/.shenfun'
 
+print(os.path.dirname(os.path.abspath(__file__)))
 locations = [os.path.dirname(os.path.abspath(__file__)),
              os.path.expanduser('~/.shenfun'),
              os.getcwd()]
