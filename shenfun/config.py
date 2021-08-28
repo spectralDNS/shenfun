a92fca88b58e01a040dd81f4e1a5fb7183143bfc
Mikael Mortensen <mikaem@math.uio.no>
2021-08-28 11:15:29 +0200

    Trying expandvars in config

--- shenfun/config.py
+++ shenfun/config.py
@@ -3,8 +3,8 @@ import yaml
 
 # The configuration file can be overloaded locally or in '~/.shenfun'
 
-print(os.path.dirname(os.path.abspath(__file__)))
-locations = [os.path.dirname(os.path.abspath(__file__)),
+print(os.path.dirname(os.path.expandvars(os.path.abspath(__file__))))
+locations = [os.path.dirname(os.path.expandvars(os.path.abspath(__file__))),
              os.path.expanduser('~/.shenfun'),
              os.getcwd()]
 
