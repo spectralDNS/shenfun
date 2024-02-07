c6738ce171a926d87b3db8c37cef4f76abf7ebda
Mikael Mortensen <mikaem@math.uio.no>
2024-02-07 13:53:28 +0100

    Switch docs theme to alabaster

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,8 +90,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 #html_theme = 'sphinxdoc'
 #html_theme = 'traditional'
 #html_theme = 'scrolls'
