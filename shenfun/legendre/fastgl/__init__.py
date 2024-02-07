b7cfcb1b3f9133bcfc6a1c86ed9cd043a759fa91
Mikael Mortensen <mikaem@math.uio.no>
2024-02-07 16:38:47 +0100

    Fixing import of fastgl

--- shenfun/legendre/fastgl/__init__.py
+++ shenfun/legendre/fastgl/__init__.py
@@ -1,1 +1,8 @@
-from .fastgl_wrap import leggauss, getGLPair
+try:
+    from . import fastgl_wrap
+except ModuleNotFoundError:
+    print('fastgl not found')
+    fastgl_wrap = None
+
+leggauss = getattr(fastgl_wrap, 'leggauss', None)
+getGLPair = getattr(fastgl_wrap, 'getGLPair', None)
