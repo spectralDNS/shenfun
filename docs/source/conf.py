47bbf6b197e3a472366ac264b8f5e329a8116292
Mikael Mortensen <mikaem@math.uio.no>
2024-02-07 14:51:32 +0100

    Try to fix conf.py to detect shenfun

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 import subprocess
 #import sphinx_bootstrap_theme
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
